965bc219b4ef969a5d343bfa66e6bffff30e9b9a
Jonathan <jonathan.23.23@icloud.com>
2026-03-26 15:59:38 -0300

    se dejan listas las dependencias de React

---
+++ frontend/sanos_y_salvos/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div>
+          <h1>Este es el front</h1>
+        </div>
+      </section>
+
+      <div className="ticks"></div>
+      <section id="spacer"></section>
+    </>
+  )
+}
+
+export default App
