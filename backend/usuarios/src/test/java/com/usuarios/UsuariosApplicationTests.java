f3845048ba2190d03fa634f2962966075e99bdd3
jonathan <jonathan.saravia.223@gmail.com>
2026-05-01 23:28:45 -0400

    Se configura Docker-compose.yml com. Se da por finalizada la gestión y programación de este MS

--- backend/usuarios/src/test/java/com/usuarios/UsuariosApplicationTests.java
+++ backend/usuarios/src/test/java/com/usuarios/UsuariosApplicationTests.java
@@ -2,12 +2,14 @@ package com.usuarios;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@org.springframework.test.context.ActiveProfiles("test")
 class UsuariosApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
