d930cbbf2e829352b2cd101aa21955e5b99e77eb
jonathan <jonathan.saravia.223@gmail.com>
2026-05-01 20:10:31 -0400

    Se dejan las configuraciones para trabajar con JWT y se hace el hasheo de claves

--- backend/usuarios/src/main/java/com/usuarios/config/SecurityConfig.java
+++ backend/usuarios/src/main/java/com/usuarios/config/SecurityConfig.java
@@ -10,6 +10,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.http.HttpMethod;
 
 import com.usuarios.security.JwtAuthFilter;
 
@@ -26,18 +27,20 @@ public class SecurityConfig {
         .authorizeHttpRequests(auth -> auth
 
             // AUTH (login público)
-            .requestMatchers("/auth/**").permitAll()
+            .requestMatchers("/auth/**").permitAll() // permitir todo lo que empiece con /auth sin autenticación
 
             // registro (para permitir creación sin token)
-            .requestMatchers("/usuarios").permitAll()
-            .requestMatchers("/usuarios/").permitAll()
+            .requestMatchers(HttpMethod.GET, "/usuarios/**").permitAll() // permitir GET sin autenticación
+            .requestMatchers(HttpMethod.POST, "/usuarios/**").permitAll() // permitir POST sin autenticación
+            .requestMatchers(HttpMethod.PUT, "/usuarios/**").permitAll() // permitir PUT sin autenticación
+            .requestMatchers(HttpMethod.DELETE, "/usuarios/**").permitAll() // permitir DELETE sin autenticación
 
             // todo lo demás protegido
             //.requestMatchers("/usuarios/**").authenticated()
 
             .anyRequest().authenticated()
-        )
-        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+        );
+        //.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
     }
