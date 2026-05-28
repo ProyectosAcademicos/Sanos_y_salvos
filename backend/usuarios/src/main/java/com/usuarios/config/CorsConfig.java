a7945b594363e33895c29d8e87af91d608a19e86
jonathan <jonathan.saravia.223@gmail.com>
2026-05-27 21:29:14 -0400

    Se genera front en contenedor docker y se modifica backend usuarios para evitar problemas de cors

--- backend/usuarios/src/main/java/com/usuarios/config/CorsConfig.java
+++ backend/usuarios/src/main/java/com/usuarios/config/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig {
         config.setAllowCredentials(true);
 
         config.setAllowedOrigins(List.of(
-                "http://localhost:5173"
+                "http://localhost:5173",
+                "http://localhost:3000"
         ));
 
         config.setAllowedHeaders(List.of("*"));
