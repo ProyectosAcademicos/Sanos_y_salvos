fee57ab44a1f5bb0fb4d8fe4cf6a1f63d8692309
xDani-R <rodriguez.cesar725@gmail.com>
2026-06-06 18:46:44 -0400

    Se modifica el archivo reportesApplicationTest

--- backend/reportes/src/test/java/com/reportes/ReportesApplicationTests.java
+++ backend/reportes/src/test/java/com/reportes/ReportesApplicationTests.java
@@ -1,13 +1,13 @@
-// package com.reportes;
+package com.reportes;
 
-// import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Test;
 // import org.springframework.boot.test.context.SpringBootTest;
 
 // @SpringBootTest
-// class ReportesApplicationTests {
+class ReportesApplicationTests {
 
-// 	@Test
-// 	void contextLoads() {
-// 	}
+	@Test
+	void contextLoads() {
+	}
 
-// }
+}
