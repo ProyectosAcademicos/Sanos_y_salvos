095f6bfaaa279386846585cd4cad2e9a8efff843
jonathan <jonathan.saravia.223@gmail.com>
2026-06-06 23:33:53 -0400

    se arreglan estilos css del registro de usuario

--- front/src/services/authService.jsx
+++ front/src/services/authService.jsx
@@ -2,10 +2,10 @@
 
 export async function login(rut, contrasena) {
 
-  console.log("ENVIANDO:");
-  console.log("rut =", rut);
-  console.log("contrasena =", contrasena);
-  
+  // console.log("ENVIANDO:");
+  // console.log("rut =", rut);
+  // console.log("contrasena =", contrasena);
+
   const response = await fetch("http://localhost:8081/auth/usuarios/login", {
     method: "POST",
     headers: {
@@ -16,10 +16,10 @@ export async function login(rut, contrasena) {
       contrasena
     })
   });
-  console.log("rut:", response.rut);
-  console.log("contrasena:", response.contrasena);
+  // console.log("rut:", response.rut);
+  // console.log("contrasena:", response.contrasena);
 
-  console.log("STATUS:", response.status);
+  // console.log("STATUS:", response.status);
 
   if (!response.ok) {
     throw new Error("Credenciales incorrectas");
