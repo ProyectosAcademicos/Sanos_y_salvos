bb69a656d82ad8294def7be17bdddfbad09d6810
jonathan <jonathan.saravia.223@gmail.com>
2026-06-07 00:13:43 -0400

    Se conecta login front y back a traves de api gateway. WIP establecer el tipo de usuario

--- front/src/componentes/RegistroContenido/RegistroContenido.jsx
+++ front/src/componentes/RegistroContenido/RegistroContenido.jsx
@@ -1,21 +1,44 @@
 import './RegistroContenido.css';
 import { useNavigate } from 'react-router-dom';
+import { register } from "../../services/registerService";
+import { useState } from 'react';
 
 // Cambiamos a Mayúscula la primera letra
 const RegistroContenido = () => {
+
+    const [rut, setRut] = useState("");
+    const [nombre, setNombre] = useState("");
+    const [email, setEmail] = useState("");
+    const [contrasena, setContrasena] = useState("");
+    const [telefono, setTelefono] = useState("");
+    const [direccion, setDireccion] = useState("");
+
     const navigate = useNavigate();
+
+    const handleRegistro = async (e) => {
+        e.preventDefault();
+        try {
+            const data = await register(rut, nombre, email, contrasena, telefono, direccion);
+            localStorage.setItem("token", data.token);
+            alert("Registro exitoso");
+            console.log("REGISTRO EJECUTADO");
+            navigate('/login');
+        } catch (error) {
+            console.error('Error al registrar usuario:', error);
+        }
+    };
 
     return (
         <div className="contenedorBody">
             <h1>Registro de usuario</h1>
-            <form className="registro-form">
+            <form className="registro-form" onSubmit={handleRegistro}>
                 <div className='contenedorCP'>
-                    <input type="text" placeholder="Rut" className="input-linea" />
-                    <input type="text" placeholder="Nombre" className="input-linea" />
-                    <input type="email" placeholder="Email" className="input-linea" />
-                    <input type="password" placeholder="Contraseña" className="input-linea" />
-                    <input type="text" placeholder="Teléfono" className="input-linea" />
-                    <input type="text" placeholder="Dirección" className="input-linea" />
+                    <input type="text" placeholder="Rut" className="input-linea" value={rut} onChange={(e) => setRut(e.target.value)} />
+                    <input type="text" placeholder="Nombre" className="input-linea" value={nombre} onChange={(e) => setNombre(e.target.value)} />
+                    <input type="email" placeholder="Email" className="input-linea" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    <input type="password" placeholder="Contraseña" className="input-linea" value={contrasena} onChange={(e) => setContrasena(e.target.value)} />
+                    <input type="text" placeholder="Teléfono" className="input-linea" value={telefono} onChange={(e) => setTelefono(e.target.value)} />
+                    <input type="text" placeholder="Dirección" className="input-linea" value={direccion} onChange={(e) => setDireccion(e.target.value)} />
                 </div>
                 <div className="contenedorBoton">
                     <button type="submit" className="boton-registrar">Registrar</button>
--- front/src/services/registerService.jsx
+++ front/src/services/registerService.jsx
@@ -1,26 +1,44 @@
 
 
-export async function register(nombre, apellido, email, contrasena) {
+export async function register(rut, nombre, email, contrasena, telefono, direccion) {
+
+
+    console.log("ENVIANDO:");
+    console.log("rut =", rut);
+    console.log("contrasena =", contrasena);
+    console.log("telefono =", telefono);
+    console.log("direccion =", direccion);
+
   const response = await fetch("http://localhost:8081/auth/usuarios/register", {
     method: "POST",
     headers: {
       "Content-Type": "application/json"
     },
     body: JSON.stringify({
+      rut,
       nombre,
-      apellido,
       email,
-      contrasena
+      contrasena,
+      telefono,
+      direccion
     })
   });
 
-  if (nombre === "" || apellido === "" || email === "" || contrasena === "") {
+  if (rut === "" || nombre === "" || email === "" || contrasena === "" || telefono === "" || direccion === "") {
     alert("Por favor, complete todos los campos");
     return;
   }
 
-  if (!response.ok) {
-    throw new Error("Error al registrar el usuario");
+  //si la respuesta es 403, mostrar un mensaje de error indicando que el usuario ya existe
+    if (response.status === 403) {
+        alert("Error al registrar usuario: El usuario ya existe");
+        throw new Error("Error al registrar usuario: El usuario ya existe");
+    }
+
+    //si aparece otro tipo de error diferente a 403, mostrar un mensaje de error indicando el status del error
+  if (!response.status !== 403) {
+    alert("Error status: " + response.statusText + " al registrar usuario. Por favor, intente nuevamente.");
+    throw new Error("Error status: " + response.statusText);
   }
 
   return response.json();
